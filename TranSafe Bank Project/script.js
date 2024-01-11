0c8e020969cda0c44ed5c1bed7072da53f319e4a
Mohit Kumar <89794081+mohitkaninwal@users.noreply.github.com>
2024-01-11 20:55:33 +0530

    Update script.js

--- TranSafe Bank Project/script.js
+++ TranSafe Bank Project/script.js
@@ -79,7 +79,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}₹</div>
+        <div class="movements__value">₹${mov}</div>
       </div>
     `;
 
@@ -89,19 +89,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}₹`;
+  labelBalance.textContent = `₹${acc.balance}`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}₹`;
+  labelSumIn.textContent = `₹${incomes}`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}₹`;
+  labelSumOut.textContent = `₹${Math.abs(out)}`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -111,7 +111,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}₹`;
+  labelSumInterest.textContent = `₹${interest}`;
 };
 
 const createUsernames = function (accs) {
